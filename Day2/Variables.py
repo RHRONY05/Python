de2bfd96ccaedd03a69bfa1c9052e206cc73acfa
RHRONY <muhammadrony147@gmail.com>
2025-07-06 16:55:19 +0600

    Day2 task complete

--- Day2/Variables.py
+++ Day2/Variables.py
@@ -1,6 +1,10 @@
 # Variables
 a = 15
 s = "rony"
+
+#A will not overwrite a, variables are case sensitive
+A = "Rony"
+print("My name is", A)
 # type conversion
 f = float(a)
 
@@ -10,7 +14,7 @@ print("a, s, f = ", a, s, f)
 print("Type of variable a", type(a))
 
 # User input--> by default input is of type string
-var = input("User input: = ")
+var = input("Enter Something it will be store on var: = ")
 print("Type of var is = ", type(var))
 print("By default user input is a string, here var is a string: ", var)
 
@@ -20,3 +24,47 @@ b = int(input("Enter number b: = "))
 
 # We can add similar datatype, otherwise it will show error
 print("a + b = ", (a+b))
+
+# Assign multiple variables together
+x, y, z = "apple", "banana", "orange"
+print("Some of the delicious fruits are: ", x ,",",y,",",z)
+
+
+# Unpacking
+fruits = ["apple", "banana", "cherry"] #list
+# Unpack list
+x, y, z = fruits
+print("Unpacking fruits:")
+print(x)
+print(y)
+print(z)
+
+
+# One value to multiple variables
+print("One value to multiple variables:")
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Global variables-> Variables declared outside of any functions are global
+# Variables used till now is global, it can be used any where
+
+def myfunc():
+   print("Acessing global variable x, y, z inside a function. x, y, z is = ", x, y, z)
+   # local variable
+   var2 = 35
+   # this can be accessed outside of the function
+   # global var3 = 55, we can't do this
+   global var3
+   var3 = 55
+   print("Var2 inside of the function: ", var2)
+
+# Calling function
+myfunc()
+
+# We can't acess variable of a function outside of that function
+# print("Var2 outside of the function: ", var2)
+
+print("Var3 outside of the function: ", var3)
+
